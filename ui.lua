251eec8f4806a52e779272c53f523f409fb1bda5
HimbeerserverDE <lpaschek@icloud.com>
2020-10-25 12:07:56 +0100

    Add test UI

---
+++ ui.lua
@@ -0,0 +1,15 @@
+bedwars.huds = {}
+
+bedwars.std_hud = {
+	hud_elem_type = "text",
+	position = {x = 0.9, y = 0.5},
+	offset = {x = 20, y = 20},
+	text = "You",
+	alignment = {x = 1, y = 1},
+	scale = {x = 100, y = 100},
+	number = 0xFFFFFF,
+}
+
+minetest.register_on_joinplayer(function(player)
+	bedwars.huds[player:get_player_name()] = player:hud_add(bedwars.std_hud)
+end)
