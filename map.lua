a2fe25bc629d1052fbd43b8ae591d9b00cb27631
HimbeerserverDE <lpaschek@icloud.com>
2020-10-27 18:01:23 +0100

    Add player death on void

--- map.lua
+++ map.lua
@@ -81,3 +81,17 @@ minetest.register_chatcommand("map_list", {
 minetest.register_on_shutdown(function()
 	bedwars.storage:set_string("maps", minetest.serialize(bedwars.maps))
 end)
+
+local timer = 0
+minetest.register_globalstep(function(dtime)
+	timer = timer + dtime
+	if timer >= 2 then
+		local players = minetest.get_connected_players()
+		for _, player in ipairs(players) do
+			if player:get_pos().y < 80 then
+				player:set_hp(0)
+			end
+		end
+		timer = 0
+	end
+end)
