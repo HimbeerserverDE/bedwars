e7cc74859be9d5ef7083c869445b6b02bec13f4b
HimbeerserverDE <lpaschek@icloud.com>
2020-11-07 21:22:04 +0100

    Remove bed remover

--- antibuild.lua
+++ antibuild.lua
@@ -1,4 +1,4 @@
-bedwars.buyable_nodes = {"wool:red", "wool:green", "wool:blue", "wool:yellow", "default:wood", "default:obsidian", "beds:bed_bottom", "tnt:tnt"}
+bedwars.buyable_nodes = {"wool:red", "wool:green", "wool:blue", "wool:yellow", "default:wood", "default:obsidian", "tnt:tnt"}
 
 bedwars.is_buyable_node = function(node)
 	for _, bnode in ipairs(bedwars.buyable_nodes) do
@@ -29,7 +29,7 @@ end)
 minetest.register_on_dignode(function(pos, oldnode, digger)
 	if not digger then return end
 	if not digger:is_player() then return end
-	if not minetest.check_player_privs(digger:get_player_name(), {build = true}) and not bedwars.is_buyable_node(oldnode) then
+	if not minetest.check_player_privs(digger:get_player_name(), {build = true}) and not bedwars.is_buyable_node(oldnode) and not oldnode.name == "beds:bed_bottom" then
 		minetest.set_node(pos, oldnode)
 		minetest.chat_send_player(digger:get_player_name(), "You can't dig this node.")
 		local inv = digger:get_inventory()
